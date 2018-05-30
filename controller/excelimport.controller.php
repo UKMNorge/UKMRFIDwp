2f8db2f0f0642e647ad516429132e1ca7d4b815b
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-05-30 12:58:35 +0000

    Excel-update

--- controller/excelimport.controller.php
+++ controller/excelimport.controller.php
@@ -19,32 +19,36 @@ if (UKM_HOSTNAME != 'ukm.dev') {
 }
 
 // Hvis vi har mottatt en fil:
-if ( null != $_FILES['excelImportFile'] ) {
-	// Global personliste 
-	$personListe = array();
-	// Pakk ut Excel-fil
-	$excelDoc = PHPExcel_IOFactory::load($_FILES['excelImportFile']['tmp_name']);
-	// Sheet number 2 atm
-	$worksheet = $excelDoc->getSheet(0);
-	// First row is headers
-	$row = 2;
-	// Loop through alle linjer der navnet ikke er tomt.
-	while ($worksheet->getCell($firstNameColumn.$row)->getValue() != "") {
+try {
+	if ( null != $_FILES['excelImportFile'] ) {
+		// Global personliste 
+		$personListe = array();
+		// Pakk ut Excel-fil
+		$excelDoc = PHPExcel_IOFactory::load($_FILES['excelImportFile']['tmp_name']);
+		$worksheet = $excelDoc->getSheet(0);
+		// First row is headers
+		$row = 2;
+		// Loop through alle linjer der navnet ikke er tomt.
+		while ($worksheet->getCell($firstNameColumn.$row)->getValue() != "") {
 
-		$person = new stdClass();
-		$person->id = $row;
-		$person->fornavn = $worksheet->getCell($firstNameColumn.$row)->getValue();
-		$person->etternavn = $worksheet->getCell($lastNameColumn.$row)->getValue();
-		$person->mobil = $worksheet->getCell($mobileColumn.$row)->getValue();
-		$person->herd = $worksheet->getCell($herdColumn.$row)->getValue();
+			$person = new stdClass();
+			$person->id = $row;
+			$person->fornavn = $worksheet->getCell($firstNameColumn.$row)->getValue();
+			$person->etternavn = $worksheet->getCell($lastNameColumn.$row)->getValue();
+			$person->mobil = $worksheet->getCell($mobileColumn.$row)->getValue();
+			$person->herd = $worksheet->getCell($herdColumn.$row)->getValue();
+			
+			$personListe[] = $person;
+
+			$row++;
+		}
 		
-		$personListe[] = $person;
-
-		$row++;
+		\UKMRFID::addViewData('filnavn', $_FILES['excelImportFile']['name']);
+		\UKMRFID::addViewData('personListe', $personListe);	
 	}
-	
-	\UKMRFID::addViewData('filnavn', $_FILES['excelImportFile']['name']);
-	\UKMRFID::addViewData('personListe', $personListe);	
+}
+catch (Exception $e) {
+	\UKMRFID::addViewData('error', array('level'=>'danger', 'message'=>"Klarte ikke å lese filen du lastet opp!"));
 }
 
 require_once(UKMRFID_INCLUDE_PATH .'herd.collection.php');

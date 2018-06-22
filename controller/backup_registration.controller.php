4d6b93ff7b972b80d1f2bf958bea3c1f4cc32526
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-06-22 20:12:04 +0000

    Postgres init

--- controller/backup_registration.controller.php
+++ controller/backup_registration.controller.php
@@ -1,7 +1,11 @@
 <?php
 namespace UKMNorge\RFID;
+use Exception;
 
 ### ETTERREGISTRERING
+require_once( 'UKM/postgres.class.php');
+POSTGRES::connect( PG_RFID_USER, PG_RFID_PASS, PG_RFID_DB );
+
 require_once(UKMRFID_INCLUDE_PATH .'person.collection.php');
 require_once(UKMRFID_INCLUDE_PATH .'area.collection.php');
 

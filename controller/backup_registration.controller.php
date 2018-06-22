aefb0b91f31ddb355bb15b8120de2b6b358e6f40
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-06-22 20:01:08 +0000

    List ut personer, kanskje?

--- controller/backup_registration.controller.php
+++ controller/backup_registration.controller.php
@@ -2,6 +2,8 @@
 namespace UKMNorge\RFID;
 
 ### ETTERREGISTRERING
+require_once(UKMRFID_INCLUDE_PATH .'person.collection.php');
 require_once(UKMRFID_INCLUDE_PATH .'area.collection.php');
 
 \UKMRFID::addViewData('areas', AreaColl::getAllByName() );
+\UKMRRIFD::addViewData('persons', PersonColl::getAll() );

2e9586eac716e072df6ccc96dd9601aae6431eed
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-06-22 20:01:46 +0000

    No typos allowed!

--- controller/backup_registration.controller.php
+++ controller/backup_registration.controller.php
@@ -6,4 +6,4 @@ require_once(UKMRFID_INCLUDE_PATH .'person.collection.php');
 require_once(UKMRFID_INCLUDE_PATH .'area.collection.php');
 
 \UKMRFID::addViewData('areas', AreaColl::getAllByName() );
-\UKMRRIFD::addViewData('persons', PersonColl::getAll() );
+\UKMRIFD::addViewData('persons', PersonColl::getAll() );

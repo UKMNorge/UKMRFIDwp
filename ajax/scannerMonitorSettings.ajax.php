0406ea3ed1661e72b99389aa8c8509ff6a5b596e
Asgeir Stavik Hustad <askhus@online.no>
2019-06-24 01:57:15 +0200

    Progress!

--- ajax/scannerMonitorSettings.ajax.php
+++ ajax/scannerMonitorSettings.ajax.php
@@ -18,22 +18,58 @@ POSTGRES::connect( PG_RFID_USER, PG_RFID_PASS, PG_RFID_DB );
 // TODO: Move upwards
 require_once(UKMRFID_INCLUDE_PATH . 'monitoraccess.collection.php');
 
-// Will only dump on next load.
-var_dump("Session_ID: ".$session_id);
-
 // Check table for list of scanners
+$scannerList = MonitorAccessColl::getForSessionId($session_id);
 
+$postedScannerList = $_POST["scannerList"];
 
 // Process posted settings:
+// Are there any scanners in the list from the frontend, and the numbers don't match up?
+if (
+	$postedScannerList != null && 
+	( count($postedScannerList) != count($scannerList)	) ) 
+{
+	// If yes, update scanner-list in monitor_access for this session_id
+	createList($postedScannerList);
+	var_dump($postedScannerList);
+	self::addResponseData('success', false);
+} elseif ($postedScannerList != null) {
+	// Also, if there is posted a scanner list and it has different ID's, update list in table
+	updateList($postedScannerList, $scannerList);
+	self::addResponseData('success', false);
 
-// Are there any scanners in the list from the frontend?
-var_dump(MonitorAccessColl::getForSessionId($session_id));
+} else {
+	// If there are no posted lists, just add the ones from the table
+	self::addResponseData('scannerList', $scannerList);
+	// Reply JSON OK to enable SSE on client-side.
+	self::addResponseData('success', true);
+}
 
-// If not, use table settings
-
-// If yes, update scanner-list in monitor_access for this session_id
-
-// Reply JSON OK to enable SSE on client-side.
-self::addResponseData('success', false);
 
 // Add list of devices currently in table
+function updateList($posted, $current) {
+	var_dump($posted);
+	$newScanners = array();
+	foreach($posted as $postedScannerID) {
+		$exists = false;
+		foreach($current as $tableScanner) {
+			if ( $tableScanner->ID == $postedScannerID) {
+				$exists = true;
+			}
+		}
+		if( !$exists ) {
+			// Add to table
+			array_push($newScanners, $postedScannerId);
+		}
+	}
+
+	// If a scanner exists in current but not in posted, it should be removed from the table.
+
+	// Actually add any if $newScanners aren't empty.
+	throw new Exception("Can't update list yet!");
+}
+
+function createList($newList) {
+	// Remove all old scanners that have the same session_id first!!!
+
+}

9e3839ee81436038368d9c20f24a3e83d3d27a3d
Asgeir Stavik Hustad <askhus@online.no>
2019-06-24 09:39:10 +0200

    Actual scanners

--- controller/scanner_monitor.controller.php
+++ controller/scanner_monitor.controller.php
@@ -7,14 +7,8 @@ use stdClass;
 require_once(UKMRFID_INCLUDE_PATH .'scanner.collection.php');
 require_once(UKMRFID_INCLUDE_PATH .'area.collection.php');
 
-// Last inn valgte scannere fra tabell monitor_access
-
 
 // Liste over alle scannere til velge-funksjon
-$scanner = new stdClass();
-$scanner->id = 1;
-$scanners = array($scanner);
+$scanners = ScannerColl::getAllByName();
 
-
-var_dump(get_include_path());
 \UKMRFID::addViewData( 'scanners', $scanners );

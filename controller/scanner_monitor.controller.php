b84ef2fe21866ed556dccb42207a512a4cf1f11d
Asgeir Stavik Hustad <askhus@online.no>
2019-06-25 11:16:13 +0200

    Moar scan

--- controller/scanner_monitor.controller.php
+++ controller/scanner_monitor.controller.php
@@ -6,9 +6,17 @@ use stdClass;
 	
 require_once(UKMRFID_INCLUDE_PATH .'scanner.collection.php');
 require_once(UKMRFID_INCLUDE_PATH .'area.collection.php');
-
+require_once( 'UKM/postgres.class.php');
+require_once(UKMRFID_INCLUDE_PATH . 'monitoraccess.collection.php');
+POSTGRES::connect( PG_RFID_USER, PG_RFID_PASS, PG_RFID_DB );
 
 // Liste over alle scannere til velge-funksjon
 $scanners = ScannerColl::getAllByName();
-
 \UKMRFID::addViewData( 'scanners', $scanners );
+
+$selectedScanners = array();
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	$session_id = $_COOKIE["session"];
+	$scannerList = MonitorAccessColl::getForSessionId($session_id);
+}
+\UKMRFID::addViewData( 'selectedScanners', $selectedScanners);
